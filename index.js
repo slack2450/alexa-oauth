5b04b3ac4cb5bb20b1a0803a010db549d70d64e5
Joss Bird <admin@plasam.xyz>
2019-08-07 18:58:09 +0100

    remove debug

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ oauthServer.grant(oauth2orize.grant.code(function(client, redirectURI, user, are
   }));
 
 oauthServer.serializeClient(function(application, done) {
-    console.log(application);
-	done(null, application.id);
+	done(null, application.title);
 });
 
 oauthServer.deserializeClient(function(id, done) {
