fd5cdc6c666a079d2292befc7ca7a8e72e54228f
Joss Bird <admin@plasam.xyz>
2019-08-07 18:31:49 +0100

    Update

--- index.js
+++ index.js
@@ -7,17 +7,30 @@ var app = express();
 
 var oauthServer = oauth2orize.createServer();
 
+oauthServer.grant(oauth2orize.grant.code(function(client, redirectURI, user, ares, done) {
+    var code = utils.uid(2);
+  
+    var ac = new AuthorizationCode(code, client.id, redirectURI, user.id, ares.scope);
+    ac.save(function(err) {
+      if (err) { return done(err); }
+      return done(null, code);
+    });
+  }));
+
 app.get('/auth/start', oauthServer.authorize(function(applicationID, redirectURI, done){
     console.log("applicationID=" + applicationID)
     console.log("redirectURI=" + redirectURI);
 }));
 
 var options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt')
+    key: fs.readFileSync('privkey.pem'),
+    cert: fs.readFileSync('cert.pem')
 };
 server = https.createServer(options, app);
 
-server.listen(443, '0.0.0.0', function(){
+const host = '0.0.0.0';
+const port = 443
+
+server.listen(port, host, function(){
 	console.log('App listening on  %s:%d!', host, port);
 });
